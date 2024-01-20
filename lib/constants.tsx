975a1a737779486458794392345a32b93517cde1
Mayank Singh <92156721+codescalper@users.noreply.github.com>
2024-01-20 18:53:22 +0000

    Add constants and data for candy machine

--- lib/constants.tsx
+++ lib/constants.tsx
@@ -1,3 +1,6 @@
 import { PublicKey } from "@solana/web3.js";
 
 export const RPC_ENDPOINT = process.env.RPC_URL || "https://api.devnet.solana.com";
+
+
+export const CANDY_MACHINE_ID =new PublicKey( process.env.CANDY_MACHINE || "h");
---
+++ lib/data.ts
@@ -0,0 +1,83 @@
+export const data = {
+    "program": {
+        "candyMachine": "9rY7xSHnmfvenkwGmweFFnEmADQXTULGDwgcczYMbjR7",
+        "candyGuard": "",
+        "candyMachineCreator": "CdmBLjRGi7RJSLDWRAmfcXEswwrXbTUry274Ko2n7F43",
+        "collectionMint": "HZUu9VLcbUW8z3RwHgzcFYxiSpNgor6ahZLGM9MRy6Hi"
+    },
+    "items": [
+        {
+            "name": "SRB #3713",
+            "image_hash": "\u000f%�V�$���\u001d�4��\u0004��J2\u001d�>\u001f�n���QC'Y",
+            "image_link": "https://arweave.net/hsYDuKbBKdMtXHgCGUL9xRFT4xyKvC9CoWLxNlXPeds",
+            "metadata_hash": "�7eRSt2b�\u0001[�G�卪����\u0002�<�\u0019c\u0017kU3B",
+            "metadata_link": "https://arweave.net/uyisiWkpLQREobsGks8lSpqXUCQUlHAgEZBuLgDSKRg",
+            "onChain": true
+          },
+         {
+            "name": "SRB #1001",
+            "image_hash": "�a���\u0019�;\u0016oA-]\b%ꁒ7��4ⷽ�:��F",
+            "image_link": "https://arweave.net/MS7aXMOgtYb86-Ap-h3qE4sbxNuPG_6qiRFdWlY4gWY",
+            "metadata_hash": "�\u001aT�\u001eY�\u0018T����(w��\u001c\u0011���L�� �R\u0001\u0016��",
+            "metadata_link": "https://arweave.net/58EMm7tE-NKE5dAK8CLRoJ2m_C87J0AHpVQZ5WA_KUA",
+            "onChain": true
+          },
+          {
+            "name": "SRB #3715",
+            "image_hash": "3v\u001a&]��3��\u0001�)��X�c%_\u001fTB�)U\u0011�YY",
+            "image_link": "https://arweave.net/T-5zTs9QnWRSQuKID4IQ1S2ECKfy_FzLFACJjbLCGAs",
+            "metadata_hash": "�.d����9$H\\��\u000e3�@����\u0002#�\u001a�M�� ��",
+            "metadata_link": "https://arweave.net/pEDTo5sjLqv1ZkWw2yS0Bh8jS4waIItmVNERwOB5SY8",
+            "onChain": true
+          },{
+            "name": "SRB #3716",
+            "image_hash": "�O6�\bna�i��i1T�.ߜ�\u001e92\u000b�h�TjA\u0003�D",
+            "image_link": "https://arweave.net/vQ_hpIFZd0NtBALVfbh6Mkpxk8Bm3tLx8_7XPNTZLac",
+            "metadata_hash": "��\"�6Bs�#B΄�w�a��)��>\n\u0019r��6�T�",
+            "metadata_link": "https://arweave.net/B18iRARApTsaWfrrXXjW5vy0NNjdz4uZ3LXh3ONoPbU",
+            "onChain": true
+          },{
+            "name": "SRB #3717",
+            "image_hash": "����\u0011��On�.UϠ�-��@�%�_pߦ\u001c�W\u0003�/",
+            "image_link": "https://arweave.net/mz8cs0ZWETwd66Damg3YNEu0o50m8lciBN9qSHGTg3o",
+            "metadata_hash": "\u0000\t�\u0017\u0016�r�_�$d��ݙ�\u0000�^�\t�Yc\t�*�G�*",
+            "metadata_link": "https://arweave.net/Pj02t-C6iF1JiFt8gIcXLyc5ATpY8Vtf930zZ_Cxy-Y",
+            "onChain": true
+          },{
+            "name": "SRB #3718",
+            "image_hash": "�x%�c�gV%�p\u0003���\u0015%�?N�\u0007�{/�u��Iz",
+            "image_link": "https://arweave.net/PZnPSqx_bNWn1PFg7yO11V4CqQeAdXQglGEAdtiaQug",
+            "metadata_hash": "|�1\u00025Y�w�\u0000y��F�o\u000f�*$\u0019_�9�p\u0017+jL\u0001�",
+            "metadata_link": "https://arweave.net/pMGc-laefUYFbbzh46KNbO0E1xnR4gTgxuL0Ku89U64",
+            "onChain": true
+          },{
+            "name": "SRB #3719",
+            "image_hash": "\bɻyH\u001b\u0019�<\fA+�(�&����\u0000{J�\n�\u001d��0_",
+            "image_link": "https://arweave.net/c9bWdKVrKPbHqUQEmvaBkfLk2B6992O3CPKfLFJz4DQ",
+            "metadata_hash": "�\\?��l����9uA��HV��<�рZ��<'�k�",
+            "metadata_link": "https://arweave.net/92mM995TZenhYodyNV6OlwyDkphx0BMpzUzay0RqMfU",
+            "onChain": true
+          },{
+            "name": "SRB #3720",
+            "image_hash": "/\u0000ǈ������7ߘ��mhRو^��e|d��w<",
+            "image_link": "https://arweave.net/MCveXWu7DJmUPNHt5SM2sbX11ujtI3lKW74v_XX4mQw",
+            "metadata_hash": "H}\u0010��\u00121\u0000�2�f��JG\u0007|a\fn���M)�Q8�$�",
+            "metadata_link": "https://arweave.net/fEhbFKRzhFZo652Nrf7E085Fy3EQhM1ZSkGmAC8LBYk",
+            "onChain": true
+          },{
+            "name": "SRB #3721",
+            "image_hash": "\u0017R�\u0010��9$����o�3@3<\u0012�\u0001#�\u000f�\u0003�7(N\u001a�",
+            "image_link": "https://arweave.net/gmckOFYowiGKexJ3DzkB87hn-WW-FG476kjWUf7XexI",
+            "metadata_hash": "�ΰ�'\u001b�$���\u001bGX\t�S^i\u0003i<���T�w�}��",
+            "metadata_link": "https://arweave.net/Kk9ZDWAJqIfrO0tIt0r-B0gyYu7hK1JQiIFE27mtsh0",
+            "onChain": true
+          },{
+            "name": "SRB #3722",
+            "image_hash": "Ӈ,�!�\u0000����d��4�Me֝���Yz\u0019Ǜ\fK�",
+            "image_link": "https://arweave.net/9MSdBdOFYQoziDjKj2VhIMftLfMWfrIvqfdgm0U47Io",
+            "metadata_hash": "yܻ=eGV��\u000b%4a��7&�\u0002�J�z�'ͽ�l�\u001da",
+            "metadata_link": "https://arweave.net/00iS4XnNAui0syd5yb6q5bTAKa9lfJ1qjMXpcYjlYxk",
+            "onChain": true
+          },
+    ]
+}
